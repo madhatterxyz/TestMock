4744c7208c8024a1f5a7a2c2c043db2435e4412f
Nicolas <Nicolas@PC-NICOLAS>
2022-07-18 22:52:48 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using RestSharp;
+using TestMock.Models;
+
+Console.WriteLine("Hello, World!");
+RestClient client = new RestClient("https://localhost:7190");
+RestRequest request = new RestRequest("User");
+List<User> response = await client.GetAsync<List<User>>(request);
+foreach (var user in response)
+{
+    Console.WriteLine(JsonConvert.SerializeObject(user));
+}
+Console.ReadLine();
